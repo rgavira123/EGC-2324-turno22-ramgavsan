a6c34104c00b74efb8e7caee9a334c3e7b81d64c
Ramón Gavira <ramgavsan@alum.us.es>
2025-01-09 17:20:41 +0100

    adad

--- decide/census/models.py
+++ decide/census/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Census(models.Model):
+    apellidos = models.CharField()
     voting_id = models.PositiveIntegerField()
     voter_id = models.PositiveIntegerField()
 
